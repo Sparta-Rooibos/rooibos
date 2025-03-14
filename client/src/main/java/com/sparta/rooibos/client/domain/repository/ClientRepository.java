1e26168e82d9ca784cd4b2a5dde89aa4beb2961c
yonghun <asqwklop12@gmail.com>
2025-03-14 21:15:48 +0900

    [feat] 업체 조회 구현

--- client/src/main/java/com/sparta/rooibos/client/domain/repository/ClientRepository.java
+++ client/src/main/java/com/sparta/rooibos/client/domain/repository/ClientRepository.java
@@ -3,9 +3,13 @@ package com.sparta.rooibos.client.domain.repository;
 import com.sparta.rooibos.client.domain.entity.Client;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 public interface ClientRepository {
     Client save(Client client);
     List<Client> findAll();
+
+    Optional<Client> findById(UUID id);
 }
---
+++ client/src/main/java/com/sparta/rooibos/client/presentation/dto/res/GetClientResponse.java
@@ -0,0 +1,12 @@
+package com.sparta.rooibos.client.presentation.dto.res;
+
+
+import com.sparta.rooibos.client.application.service.dto.res.GetClientApplicationResponse;
+
+import java.time.LocalDateTime;
+
+public record GetClientResponse(String id, String name, String address, String type, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public GetClientResponse(GetClientApplicationResponse client) {
+        this(client.id(), client.name(), client.address(), client.type(), client.createdAt(), client.updatedAt());
+    }
+}
