da3f159093fae86714afd385f684325312621a2d
poorm <315luz10@gmail.com>
2025-03-15 04:37:21 +0900

    [feat] auth infrastructure 계층_레디스 로직
    목적 1) 로그인을 위한 데이터 동기화
    목적 2) 토큰 탈취 대응을 위한 블랙리스트
    
    - Blacklist 서비스
    - 사용자 정보 캐싱, 조회, 삭제 서비스

---
+++ auth/src/main/java/com/spring/cloud/client/auth/infrastructure/redis/AuthCacheService.java
@@ -0,0 +1,40 @@
+package com.spring.cloud.client.auth.infrastructure.redis;
+
+
+import com.spring.cloud.client.auth.application.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class AuthCacheService {
+    private final RedisTemplate<String, UserDTO> redisTemplate;
+    private final BlacklistService blacklistService;
+
+    private static final String CACHE_PREFIX = "auth:user:";
+    private static final long CACHE_TTL = 30;
+
+    // 사용자 정보 캐싱
+    public void createUserInfo(UserDTO user) {
+        redisTemplate.opsForValue().set(CACHE_PREFIX + user.getUsername(), user, CACHE_TTL, TimeUnit.MINUTES);
+    }
+
+    // 사용자 정보 조회
+    public Optional<UserDTO> getUserInfo(String username) {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(CACHE_PREFIX + username));
+    }
+
+    // 특정 유저 캐시 삭제
+    public void deleteUserCache(String username) {
+        redisTemplate.delete(CACHE_PREFIX + username);
+    }
+
+     // 블랙리스트 확인 기능 추가
+    public boolean isTokenBlacklisted(String token) {
+        return blacklistService.isTokenBlacklisted(token);
+    }
+}
