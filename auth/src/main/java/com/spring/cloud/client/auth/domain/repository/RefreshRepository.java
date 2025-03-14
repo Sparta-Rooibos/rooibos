20e2923026f16c81bed3fbdd94b5534fed4d0cf3
poorm <315luz10@gmail.com>
2025-03-15 04:36:25 +0900

    [feat] 리프레시 도메인 작성
    - 리프레시 엔티티
    - 리프레시 리포지토리

---
+++ auth/src/main/java/com/spring/cloud/client/auth/domain/repository/RefreshRepository.java
@@ -0,0 +1,8 @@
+package com.spring.cloud.client.auth.domain.repository;
+
+import com.spring.cloud.client.auth.domain.entity.Refresh;
+
+public interface RefreshRepository {
+    boolean deleteByRefresh(String refresh);
+    Refresh save(Refresh refresh);
+}
