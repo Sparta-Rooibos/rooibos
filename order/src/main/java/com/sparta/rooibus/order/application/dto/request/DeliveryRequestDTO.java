7241f605921c0da94764ce376a7fe71003d5826f
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-03-15 20:10:08 +0900

    [refactor] Order create 관련 파일 리팩토링
    폴더 구조 맞추기, 페인 클라이언트 적용하면 바로 되도록 준비

--- order/src/main/java/com/sparta/rooibus/order/application/dto/request/DeliveryRequestDTO.java
+++ order/src/main/java/com/sparta/rooibus/order/application/dto/request/DeliveryRequestDTO.java
@@ -4,14 +4,13 @@ import com.sparta.rooibus.order.domain.entity.Order;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
-public record FeignDeliveryRequestDTO(
-    @NotNull UUID orderID,
+public record DeliveryRequestDTO(
+    @NotNull UUID orderID,           // 주문 ID
     @NotNull UUID requestClientId,   // 공급 업체 ID
     @NotNull UUID responseClientId,  // 수령 업체 ID
     @NotNull UUID productId          // 상품 ID
 ) {
-
-    public FeignDeliveryRequestDTO(Order order) {
+    public DeliveryRequestDTO(Order order) {
         this(
             order.getId(),
             order.getRequestClientId(),
