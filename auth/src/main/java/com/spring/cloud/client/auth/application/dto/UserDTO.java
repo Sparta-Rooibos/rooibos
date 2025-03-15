bed638aa0bc8b1a5e0cf155329b82fea29e2cd2e
poorm <315luz10@gmail.com>
2025-03-16 05:47:03 +0900

    [refactor] DTO 컨벤션 통일

--- auth/src/main/java/com/spring/cloud/client/auth/application/dto/UserDTO.java
+++ auth/src/main/java/com/spring/cloud/client/auth/application/dto/UserDTO.java
@@ -1,17 +1,9 @@
 package com.spring.cloud.client.auth.application.dto;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class UserDTO {
-    private String requestId;  // Kafka 요청 ID (비동기 응답 매칭용)
-    private String username;   // 사용자 ID
-    private String password;   // 암호화된 비밀번호
-    private String role;       // 사용자 역할 (예: ROLE_USER, ROLE_ADMIN)
-}
+public record UserDTO (
+        String requestId,  // Kafka 요청 ID (비동기 응답 매칭용)
+        String username,   // 사용자 ID
+        String password,   // 암호화된 비밀번호
+        String role       // 사용자 역할 (예: ROLE_USER, ROLE_ADMIN)
+){}
 
