30b277fe550d9edb89f535a55a207e1ce5dd0410
poorm <315luz10@gmail.com>
2025-03-16 05:52:50 +0900

    [refactor] Kafka 토픽 네이밍 변경

--- auth/src/main/java/com/spring/cloud/client/auth/infrastructure/kafka/config/KafkaConsumerConfig.java
+++ auth/src/main/java/com/spring/cloud/client/auth/infrastructure/kafka/config/KafkaConsumerConfig.java
@@ -22,7 +22,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, UserDTO> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "auth-service-group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "auth-service");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
