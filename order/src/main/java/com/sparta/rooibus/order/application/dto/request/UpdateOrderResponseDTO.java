f28bd7ff1afc69f343651b22cadb2ba17025eb88
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-03-15 21:17:49 +0900

    [refactor] 주석 추가

--- order/src/main/java/com/sparta/rooibus/order/application/dto/request/UpdateOrderResponseDTO.java
+++ order/src/main/java/com/sparta/rooibus/order/application/dto/request/UpdateOrderResponseDTO.java
@@ -5,14 +5,14 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record UpdateOrderResponseDTO(
-    UUID orderId,
+    UUID orderId,           // 주문 ID
     UUID requestClientId,   // 공급 업체 ID
     UUID responseClientId,  // 수령 업체 ID
     UUID productId,         // 상품 ID
-    UUID deliveryId,
-    int quantity,            // 수량
-    String requirement,
-    LocalDateTime updatedAt
+    UUID deliveryId,        // 배송 ID
+    int quantity,           // 수량
+    String requirement,     // 요구사항
+    LocalDateTime updatedAt // 수정일
 ) {
     public UpdateOrderResponseDTO(Order order){
         this(
