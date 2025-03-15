eff05181f0214e8160b01f34b662ea2d3fac1c48
poorm <315luz10@gmail.com>
2025-03-16 06:46:04 +0900

    [feat] 롤체킹 aop 구현

--- common/src/main/java/com/sparta/rooibos/common/utils/JwtRoleParser.java
+++ common/src/main/java/com/sparta/rooibos/common/utils/JwtRoleParser.java
@@ -1,13 +1,16 @@
 package com.sparta.rooibos.common.utils;
 
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 
+@Component
 public class JwtRoleParser {
     private final SecretKey secretKey;
 
