d1e208e12919b96e50f04e8f86374c07bdd29c84
yonghun <asqwklop12@gmail.com>
2025-03-16 18:59:36 +0900

    [feat] 상품 단일 조회 기능 구현

--- product/src/main/java/com/sparta/rooibos/product/application/service/ProductService.java
+++ product/src/main/java/com/sparta/rooibos/product/application/service/ProductService.java
@@ -2,10 +2,13 @@ package com.sparta.rooibos.product.application.service;
 
 import com.sparta.rooibos.product.application.dto.request.CreateProductApplicationRequest;
 import com.sparta.rooibos.product.application.dto.response.CreateProductApplicationResponse;
+import com.sparta.rooibos.product.application.dto.response.GetProductApplicationResponse;
 import com.sparta.rooibos.product.domain.entity.Product;
 import com.sparta.rooibos.product.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +20,9 @@ public class ProductService {
 
     }
 
-    public void getProduct(String productId) {
+    public GetProductApplicationResponse getProduct(UUID productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("제공하는 상품이 존재하지 않습니다."));
+        return new GetProductApplicationResponse(product);
 
     }
 
--- product/src/main/java/com/sparta/rooibos/product/domain/repository/ProductRepository.java
+++ product/src/main/java/com/sparta/rooibos/product/domain/repository/ProductRepository.java
@@ -3,7 +3,11 @@ package com.sparta.rooibos.product.domain.repository;
 import com.sparta.rooibos.product.domain.entity.Product;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface ProductRepository {
     Product save(Product product);
+    Optional<Product> findById(UUID id);
 }
