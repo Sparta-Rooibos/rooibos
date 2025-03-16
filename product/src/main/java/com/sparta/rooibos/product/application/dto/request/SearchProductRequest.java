1b4588b8b9a5bf195a28da1b5fd852402cf7892c
yonghun <asqwklop12@gmail.com>
2025-03-16 23:04:40 +0900

    [feat] product: 동적 쿼리 할당

---
+++ product/src/main/java/com/sparta/rooibos/product/application/dto/request/SearchProductRequest.java
@@ -0,0 +1,8 @@
+package com.sparta.rooibos.product.application.dto.request;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public record SearchProductRequest(UUID id, String name, Pageable pageable) {
+}
---
+++ product/src/main/java/com/sparta/rooibos/product/domain/repository/QueryProductRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.rooibos.product.domain.repository;
+
+import com.sparta.rooibos.product.domain.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface QueryProductRepository {
+    Page<Product> getProductList(Pageable pageable, UUID id, String name);
+}
