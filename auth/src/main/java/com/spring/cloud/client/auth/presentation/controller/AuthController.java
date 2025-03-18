03784a33bc81edec43f172cff61283a4af758f16
poorm <315luz10@gmail.com>
2025-03-19 03:25:19 +0900

    [refactor] 컨트롤러 컨벤션 통일

--- auth/src/main/java/com/spring/cloud/client/auth/presentation/controller/AuthController.java
+++ auth/src/main/java/com/spring/cloud/client/auth/presentation/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.spring.cloud.client.auth.presentation.controller;
 
-import com.spring.cloud.client.auth.application.service.AuthService;
 import com.spring.cloud.client.auth.application.dto.request.LoginRequest;
+import com.spring.cloud.client.auth.application.service.port.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +18,30 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
-        return authService.login(loginRequest, request, response);
+    public ResponseEntity<Void> login(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        authService.login(loginRequest, request, response);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
-        return authService.logout(request, response);
+    public ResponseEntity<Void> logout(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        authService.logout(request, response);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/reissue")
-    public ResponseEntity<?> reissueToken(HttpServletRequest request, HttpServletResponse response) {
-        return authService.reissueToken(request, response);
+    public ResponseEntity<Void> reissueToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        authService.reissueToken(request, response);
+        return ResponseEntity.ok().build();
     }
 }
