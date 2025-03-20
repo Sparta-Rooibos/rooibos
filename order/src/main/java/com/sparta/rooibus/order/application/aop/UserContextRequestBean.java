91e5641abb66d2661d28dfde906762144f63ff3d
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-03-20 17:36:31 +0900

    [refactor] 헤더에서 유저 정보 저장해서 쓰는 aop 기능 추가

---
+++ order/src/main/java/com/sparta/rooibus/order/application/aop/UserContextRequestBean.java
@@ -0,0 +1,26 @@
+package com.sparta.rooibus.order.application.aop;
+
+import java.util.UUID;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request",proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class UserContextRequestBean {
+
+    private UUID userId;
+    private String role;
+
+    public void set(String role, UUID userId) {
+        this.role = role;
+        this.userId = userId;
+    }
+    public String getRole() {
+        return role;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+}
