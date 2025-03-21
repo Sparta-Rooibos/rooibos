d205b3ee4670d2b4dd970ff4b0768b4b12d67c7a
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-03-21 17:55:37 +0900

    [refactor] 배송 상태 enum 수정

--- delivery/src/main/java/com/sparta/rooibus/delivery/domain/model/DeliveryStatus.java
+++ delivery/src/main/java/com/sparta/rooibus/delivery/domain/model/DeliveryStatus.java
@@ -1,22 +1,11 @@
 package com.sparta.rooibus.delivery.domain.model;
 
 public enum DeliveryStatus {
-    PENDING("대기중"),  // 배송 대기 중
-    IN_PROGRESS("배송 중"),  // 배송 진행 중
-    COMPLETED("배송 완료"),  // 배송 완료
-    CANCELED("배송 취소"),  // 배송 취소
-    FAILED("배송 실패");  // 배송 실패
-
-    private final String description;
-
-    // 생성자
-    DeliveryStatus(String description) {
-        this.description = description;
-    }
-
-    // 상태 설명을 반환하는 메서드
-    public String getDescription() {
-        return description;
-    }
+        HUB_WAITING,       // 허브 대기중
+        HUB_MOVING,        // 허브 에서 허브로 이동중
+        ARRIVAL_HUB_ARRIVED,  // 목적지 허브 도착
+        DELIVERING,        // 배송중
+        DELIVERED          // 배송 완료
 }
+
 
