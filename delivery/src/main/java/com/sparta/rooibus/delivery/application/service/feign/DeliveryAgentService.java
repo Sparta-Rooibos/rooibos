9712b9d1b6cee0492b3353776bb9bb29de7b0c94
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-03-21 23:05:23 +0900

    [feat] 배송 담당자 서비스 페인 클라이언트 내용추가

--- delivery/src/main/java/com/sparta/rooibus/delivery/application/service/feign/DeliveryAgentService.java
+++ delivery/src/main/java/com/sparta/rooibus/delivery/application/service/feign/DeliveryAgentService.java
@@ -1,11 +1,15 @@
 package com.sparta.rooibus.delivery.application.service.feign;
 
 import com.sparta.rooibus.delivery.application.dto.request.feign.deliverAgent.GetDeliverRequest;
+import com.sparta.rooibus.delivery.application.dto.request.feign.deliverAgent.GetHubDeliverRequest;
 import com.sparta.rooibus.delivery.application.dto.response.feign.deliverAgent.GetDeliverResponse;
+import com.sparta.rooibus.delivery.application.dto.response.feign.deliverAgent.GetHubDeliverResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface DeliveryAgentService {
 
     GetDeliverResponse getDeliver(GetDeliverRequest from);
+
+    GetHubDeliverResponse getHubDeliver(GetHubDeliverRequest request);
 }
