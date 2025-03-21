d573b4e4c56631ef4380f70b47a429687e355b00
yonghun <asqwklop12@gmail.com>
2025-03-21 18:18:55 +0900

    [feat] 권한 롤 체킹 추가

--- client/src/main/java/com/sparta/rooibos/client/presentation/controller/ClientController.java
+++ client/src/main/java/com/sparta/rooibos/client/presentation/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.sparta.rooibos.client.presentation.controller;
 
 
+import com.sparta.rooibos.client.application.annotation.RoleCheck;
 import com.sparta.rooibos.client.application.dto.request.CreateClientRequest;
 import com.sparta.rooibos.client.application.dto.request.SearchClientRequest;
 import com.sparta.rooibos.client.application.dto.request.UpdateClientRequest;
@@ -9,6 +10,7 @@ import com.sparta.rooibos.client.application.dto.response.CreateClientResponse;
 import com.sparta.rooibos.client.application.dto.response.GetClientResponse;
 import com.sparta.rooibos.client.application.dto.response.SearchClientResponse;
 import com.sparta.rooibos.client.application.service.ClientService;
+import com.sparta.rooibos.client.application.type.Role;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,36 +25,65 @@ public class ClientController {
     private final ClientService clientService;
 
     @GetMapping
+    @RoleCheck({Role.MASTER, Role.HUB, Role.DELIVERY, Role.CLIENT})
     public ResponseEntity<SearchClientResponse> getClientList(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") Role role,
             @ModelAttribute SearchClientRequest request) {
-       return ResponseEntity.ok(clientService.searchClient(request));
+        return ResponseEntity.ok(clientService.searchClient(request));
     }
 
     @GetMapping("/{clientId}")
-    public ResponseEntity<GetClientResponse> getClient(@PathVariable UUID clientId) {
+    @RoleCheck({Role.MASTER, Role.HUB, Role.DELIVERY, Role.CLIENT})
+    public ResponseEntity<GetClientResponse> getClient(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") Role role,
+            @PathVariable UUID clientId) {
         return ResponseEntity.ok(clientService.getClient(clientId));
     }
 
     @PostMapping()
-    public ResponseEntity<CreateClientResponse> createClient(@RequestBody @Valid CreateClientRequest createClientRequest) {
+    @RoleCheck({Role.MASTER, Role.HUB})
+    public ResponseEntity<CreateClientResponse> createClient(
+            @RequestHeader(name = "X-User-Email") String email,
+            @RequestHeader(name = "X-User-Name") String username,
+            @RequestHeader(name = "X-User-Role") String role,
+            @RequestBody @Valid CreateClientRequest createClientRequest) {
         return ResponseEntity.ok(clientService.createClient(createClientRequest));
 
     }
 
     @PutMapping("/{clientId}")
-    public ResponseEntity<Boolean> updateClient(@PathVariable UUID clientId, @RequestBody UpdateClientRequest request) {
+    @RoleCheck({Role.MASTER, Role.HUB, Role.CLIENT})
+    public ResponseEntity<Boolean> updateClient(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @PathVariable UUID clientId, @RequestBody UpdateClientRequest request) {
         return ResponseEntity.ok(clientService.updateClient(clientId, request));
     }
 
     @PatchMapping("/{clientId}")
-    public ResponseEntity<Boolean> deleteClient(@PathVariable UUID clientId) {
+    @RoleCheck({Role.MASTER, Role.HUB})
+    public ResponseEntity<Boolean> deleteClient(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @PathVariable UUID clientId) {
         return ResponseEntity.ok(clientService.deleteClient(clientId));
     }
 
     //사용 허브 변경
     @Deprecated
+    @RoleCheck(Role.MASTER)
     @PatchMapping("/change/hub/{clientId}")
-    public ResponseEntity<Boolean> changeUsedHub(@PathVariable UUID clientId, @RequestBody UpdateHubIdRequest request) {
+    public ResponseEntity<Boolean> changeUsedHub(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @PathVariable UUID clientId, @RequestBody UpdateHubIdRequest request) {
         return ResponseEntity.ok(clientService.changeUsedHub(clientId, request));
     }
 }
