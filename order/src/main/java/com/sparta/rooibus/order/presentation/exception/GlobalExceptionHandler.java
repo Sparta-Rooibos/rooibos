098578055f451790321ada7ef7ed41c1c52506ef
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-03-21 11:27:14 +0900

    [refactor] 에러 처리 기능 추가

--- order/src/main/java/com/sparta/rooibus/order/presentation/exception/GlobalExceptionHandler.java
+++ order/src/main/java/com/sparta/rooibus/order/presentation/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.sparta.rooibus.order.application.exception.BusinessOrderException;
 import com.sparta.rooibus.order.application.exception.custom.OrderErrorCode;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +16,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessOrderException.class)
     public ResponseEntity<?> handleException(Exception e) {
         log.error("[orderException] error :", e);
-        return new ResponseEntity<>(new ErrorResponse(OrderErrorCode.ORDER_NOT_FOUND.getCode(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ErrorResponse(OrderErrorCode.ORDER_NOT_FOUND.getCode(), e.getMessage()), OrderErrorCode.ORDER_NOT_FOUND.getStatus());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(Exception e) {
+        log.error("[orderException] error :", e);
+        return new ResponseEntity<>(new ErrorResponse(OrderErrorCode.BAD_REQUEST_ERROR.getCode(), e.getMessage()), OrderErrorCode.BAD_REQUEST_ERROR.getStatus());
     }
 
         private record ErrorResponse(String errorCode, String message) {
