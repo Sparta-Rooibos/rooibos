c9474675104657bf0671afb87620586e37d98dee
poorm <315luz10@gmail.com>
2025-03-21 20:49:20 +0900

    [refactor] auth 에러 핸들링 불필요 로직 제거

--- auth/src/main/java/com/spring/cloud/client/auth/application/exception/custom/CommonErrorCode.java
+++ auth/src/main/java/com/spring/cloud/client/auth/application/exception/custom/CommonErrorCode.java
@@ -11,8 +11,6 @@ public enum CommonErrorCode implements ErrorCodeProvider {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COM-01", "Internal Server Error"),
     JSON_PARSING_ERROR(HttpStatus.BAD_REQUEST,"COM-02","JSON 파싱 에러"),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND,"COM-03","Resource not found"),
-    INVALID_CODE(HttpStatus.BAD_REQUEST,"COM-04","Invalid code"),
     SQL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COM-05","SQL 에러");
 
     private final HttpStatus status;
