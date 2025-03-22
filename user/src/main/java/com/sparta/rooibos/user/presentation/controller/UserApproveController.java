4a5be475650e37b581931710c425cdcce63275c4
poorm <315luz10@gmail.com>
2025-03-23 04:43:29 +0900

    [refactor] 유저 권한 승인 처리 로직
    - 기존: 유저 레디스 스트림 유저 생성 시에 전송
    - 변경: 유저 레디스 스트림 마스터가 유저 생성 시 & 유저 승인 시에 전

---
+++ user/src/main/java/com/sparta/rooibos/user/presentation/controller/UserApproveController.java
@@ -0,0 +1,30 @@
+package com.sparta.rooibos.user.presentation.controller;
+
+import com.sparta.rooibos.user.application.service.port.UserApproveService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class UserApproveController {
+    private final UserApproveService userApproveService;
+
+    @PatchMapping("/approve/{userId}")
+    public ResponseEntity<Void> approveUser(@PathVariable UUID userId) {
+        userApproveService.approveUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/reject/{userId}")
+    public ResponseEntity<Void> rejectUser(@PathVariable UUID userId) {
+        userApproveService.rejectUser(userId);
+        return ResponseEntity.ok().build();
+    }
+}
