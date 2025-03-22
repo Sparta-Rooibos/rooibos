cf31bee72950f75dd3f12e48e0c5769cdf884c8b
yonghun <asqwklop12@gmail.com>
2025-03-22 20:42:36 +0900

    [feat] 권한체킹 추가

--- Message/src/main/java/com/sparta/rooibos/message/presentation/controller/MessageController.java
+++ Message/src/main/java/com/sparta/rooibos/message/presentation/controller/MessageController.java
@@ -1,9 +1,11 @@
 package com.sparta.rooibos.message.presentation.controller;
 
+import com.sparta.rooibos.message.application.annotation.RoleCheck;
 import com.sparta.rooibos.message.application.dto.request.CreateMessageRequest;
 import com.sparta.rooibos.message.application.dto.request.SearchMessageRequest;
 import com.sparta.rooibos.message.application.dto.response.*;
 import com.sparta.rooibos.message.application.service.MessageService;
+import com.sparta.rooibos.message.application.type.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,20 +19,40 @@ public class MessageController {
     private final MessageService messageService;
 
     @PostMapping
-    public ResponseEntity<CreateMessageResponse> createMessage(@RequestBody CreateMessageRequest request) {
+    @RoleCheck({Role.MASTER, Role.HUB, Role.DELIVERY, Role.CLIENT})
+    public ResponseEntity<CreateMessageResponse> createMessage(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @RequestBody CreateMessageRequest request) {
         return ResponseEntity.ok(messageService.createMessage(request));
     }
     @GetMapping
-    public ResponseEntity<SearchMessageResponse> searchMessage(@ModelAttribute SearchMessageRequest request) {
+    @RoleCheck({Role.MASTER})
+    public ResponseEntity<SearchMessageResponse> searchMessage(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @ModelAttribute SearchMessageRequest request) {
         return ResponseEntity.ok(messageService.searchMessage(request));
     }
     @GetMapping("/{messageId}")
-    public ResponseEntity<GetMessageResponse> getMessage(@PathVariable UUID messageId) {
+    @RoleCheck({Role.MASTER})
+    public ResponseEntity<GetMessageResponse> getMessage(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @PathVariable UUID messageId) {
         return ResponseEntity.ok(messageService.getMessage(messageId));
     }
 
     @PatchMapping("/{messageId}")
-    public ResponseEntity<Void> deleteMessage(@PathVariable UUID messageId) {
+    @RoleCheck({Role.MASTER})
+    public ResponseEntity<Void> deleteMessage(
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Name") String username,
+            @RequestHeader("X-User-Role") String role,
+            @PathVariable UUID messageId) {
         messageService.deleteMessage(messageId);
         return ResponseEntity.noContent().build();
     }
