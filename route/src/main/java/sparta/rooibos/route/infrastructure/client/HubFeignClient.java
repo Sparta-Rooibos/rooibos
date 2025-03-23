3ad9e6490d2debbcc50114efc9f75b88cd0971f1
zun <k5orkrj@gmail.com>
2025-03-23 22:01:22 +0900

    [feat] Route 에서 Hub 위도 경도 정보를 받기 위해 Hub 호출 로직 보완

--- route/src/main/java/sparta/rooibos/route/infrastructure/client/HubFeignClient.java
+++ route/src/main/java/sparta/rooibos/route/infrastructure/client/HubFeignClient.java
@@ -2,13 +2,17 @@ package sparta.rooibos.route.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import sparta.rooibos.route.application.dto.response.Hub.HubClientResponse;
 
 import java.util.UUID;
 
 @FeignClient(name = "hub-service")
 public interface HubFeignClient {
 
-    @GetMapping("/api/v1/hub")
-    UUID getHubIdByRegion(@RequestParam String region);
+    @GetMapping("/api/v1/hub/{hubId}")
+    HubClientResponse getHubByHubId(@PathVariable UUID hubId);
+
+    @GetMapping("/api/v1/hub/{region}")
+    HubClientResponse getHubByRegion(@PathVariable String region);
 }
