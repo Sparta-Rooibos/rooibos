8a537258d01f02b97b3c722bc7bb8ad14c5add83
zun <k5orkrj@gmail.com>
2025-03-24 00:38:13 +0900

    [fix] Route naver map api json 파싱 에러 해결

--- route/src/main/java/sparta/rooibos/route/application/dto/request/direction/GetGeoDirectionRequest.java
+++ route/src/main/java/sparta/rooibos/route/application/dto/request/direction/GetGeoDirectionRequest.java
@@ -1,8 +1,10 @@
 package sparta.rooibos.route.application.dto.request.direction;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record GetGeoDirectionRequest(
-        String start,
-        String goal
+        @JsonProperty("start") String start,
+        @JsonProperty("goal") String goal
 ) {
     public static GetGeoDirectionRequest of(String start, String goal) {
         return new GetGeoDirectionRequest(start, goal);
--- route/src/main/java/sparta/rooibos/route/application/dto/response/direction/GetGeoDirectionResponse.java
+++ route/src/main/java/sparta/rooibos/route/application/dto/response/direction/GetGeoDirectionResponse.java
@@ -1,28 +1,42 @@
 package sparta.rooibos.route.application.dto.response.direction;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
+@Slf4j
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GetGeoDirectionResponse(
-        Route route
+        @JsonProperty("code") int code,
+        @JsonProperty("message") String message,
+        @JsonProperty("currentDateTime") String currentDateTime,
+        @JsonProperty("route") Route route
 ) {
     public Integer getDistance() {
-        return route.traoptimalList.get(0).summary.distance;
+
+        return route().traoptimal().get(0).summary().distance(); // 미터
     }
 
     public Integer getDuration() {
-        return route.traoptimalList.get(0).summary.duration;
+        return route().traoptimal().get(0).summary().duration() / 1000; // 밀리초
     }
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record Route(
-            List<Traoptimal> traoptimalList
+            List<Traoptimal> traoptimal
     ) {
+        @JsonIgnoreProperties(ignoreUnknown = true)
         public record Traoptimal(
-                Summary summary
+                @JsonProperty("summary") Summary summary
         ) {
+            @JsonIgnoreProperties(ignoreUnknown = true)
             public record Summary(
-                    Integer distance,
-                    Integer duration
-            ) {}
+                    @JsonProperty("distance") Integer distance,
+                    @JsonProperty("duration") Integer duration
+            ) {
+            }
         }
     }
 }
