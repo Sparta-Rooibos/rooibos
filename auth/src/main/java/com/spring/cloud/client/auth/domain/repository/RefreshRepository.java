a50dbd94c686b8a0f260841cc854f6b5047234ca
poorm <315luz10@gmail.com>
2025-03-26 07:29:52 +0900

    [refactor] 로그아웃 로직 변경

--- auth/src/main/java/com/spring/cloud/client/auth/domain/repository/RefreshRepository.java
+++ auth/src/main/java/com/spring/cloud/client/auth/domain/repository/RefreshRepository.java
@@ -5,5 +5,5 @@ import com.spring.cloud.client.auth.domain.entity.Refresh;
 public interface RefreshRepository {
     boolean deleteByRefresh(String refresh);
     Refresh save(Refresh refresh);
-    boolean deleteByEmail(String email);
+    void deleteByEmail(String email);
 }
