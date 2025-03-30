0c9609be1888bd731eb566ed0c0e2505a47653e0
yonghun <asqwklop12@gmail.com>
2025-03-31 00:37:35 +0900

    [fix] Deliverer 실행되지 않는 않는 이슈 수정

--- deliverer/src/main/java/com/sparta/rooibos/deliverer/infrastructure/persistence/JpaDelivererRepository.java
+++ deliverer/src/main/java/com/sparta/rooibos/deliverer/infrastructure/persistence/JpaDelivererRepository.java
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface JpaDelivererRepository extends JpaRepository<Deliverer, UUID> {
     Optional<Deliverer> findById(UUID id);
     @Query("SELECT COALESCE(MAX(d.order), -1) FROM Deliverer d WHERE d.hubId = :hubId AND d.hidden = false")
     int findMaxOrderByHubId(UUID hubId);
     int countByHubIdAndHiddenFalse(UUID hubId);
     int countByHubIdAndTypeAndHiddenFalse(UUID hubId, DelivererType type);
+    @Query("SELECT d from Deliverer d where d.hubId = :hubId and d.type = :type")
     Optional<Deliverer> findNextAvailableDeliverer(UUID hubId, DelivererType type);
 }
