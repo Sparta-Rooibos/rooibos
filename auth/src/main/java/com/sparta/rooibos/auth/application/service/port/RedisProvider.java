72db3b4f76771a9cd898f7ec3fe1dc733db84f36
poorm <315luz10@gmail.com>
2025-03-31 12:05:34 +0900

    [refactor] auth 수정

--- auth/src/main/java/com/sparta/rooibos/auth/application/service/port/RedisProvider.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.rooibos.auth.application.service.port;
-
-import java.util.Optional;
-
-public interface RedisProvider {
-    void createUserInfo(UserAuthDTO userAuthDTO);
-    Optional<UserAuthDTO> getUserInfo(String email);
-    void deleteUserInfo(String email);
-}
