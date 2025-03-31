4cd85466035a8c6e3b57a5b234cdb6a18da48f2e
poorm <315luz10@gmail.com>
2025-03-31 12:05:08 +0900

    [refactor] auth 수정

--- auth/src/main/java/com/sparta/rooibos/auth/application/dto/UserStreamEvent.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.rooibos.auth.application.dto;
-
-public record UserStreamEvent(
-        String eventType,
-        Object payload
-) {}
-
--- auth/src/main/java/com/sparta/rooibos/auth/application/service/port/RedisProvider.java
+++ auth/src/main/java/com/sparta/rooibos/auth/application/service/port/RedisProvider.java
@@ -1,7 +1,5 @@
 package com.sparta.rooibos.auth.application.service.port;
 
-import com.sparta.rooibos.auth.application.dto.UserAuthDTO;
-
 import java.util.Optional;
 
 public interface RedisProvider {
